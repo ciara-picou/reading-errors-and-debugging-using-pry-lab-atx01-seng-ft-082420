149f024852722bce252369f214b3f74f64f09339
Ciara Picou <ciarapicou@att.net>
2020-07-16 19:49:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,6 @@
 require 'pry'
 
 def generate_star_date
-  binding.pry
   (rand(100000) + 400000) / 10.0
 end
 
@@ -10,6 +9,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(star_date)
-  puts state_log(star_date.to_s)
+def engage
+  puts state_log(date)
+  date = generate_star_date
 end
