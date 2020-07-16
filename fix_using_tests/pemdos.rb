cce97dd3e97cdaacf9deeecaad456b62235bcb19
Ciara Picou <ciarapicou@att.net>
2020-07-16 19:34:37 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,10 @@
 
-String == "Say something"
-def snake_it_up(String)
-  if String[0] == "s"
+
+def snake_it_up(string = "say something")
+  if string[0] == "s"
   puts "Sssssssss" + string
  
   else
-    puts String
+    puts string
   end
 end
