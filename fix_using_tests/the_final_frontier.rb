b366aa27984c85e26d561218ec060a2f85afd991
Ciara Picou <ciarapicou@att.net>
2020-07-16 19:59:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,1 @@
 
-
-generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
-
-star_date = 49250.7
-
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
-end
-
-def engage
-  puts "Captain's Log, star date 49250.7."
-end
