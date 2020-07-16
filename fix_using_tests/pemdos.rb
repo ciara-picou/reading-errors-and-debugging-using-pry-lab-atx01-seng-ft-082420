b1bebb67febdfb24e4b91ab2d8534d534abf8e32
Ciara Picou <ciarapicou@att.net>
2020-07-16 20:06:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,11 @@
 
 
 def snake_it_up(string)
-  if string[0] == "s" || "S"
+  if string[0] == "s" 
   p "Sssssssss" + string
+  
+  elsif string[0] == "S"
+  Ssssssssss + string.shift()
  
   else
     p string
