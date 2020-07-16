9878481dc75edd4bfdc87b36620bdeef895d5e4c
Ciara Picou <ciarapicou@att.net>
2020-07-16 20:10:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,12 +2,12 @@
 
 def snake_it_up(string)
   if string[0] == "s" 
-  p "Sssssssss" + string
+  puts "Sssssssss" + string
   
   elsif string[0] == "S"
-  "Ssssssssss" + string.downcase
+  puts "Ssssssssss" + string.downcase
  
   else
-    p string
+    puts string
   end
 end
